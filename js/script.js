c8082f67188567e2eddc99ec0cf6a7d97efb0720
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-27 14:19:12 -0700

    logic to show back project dialog

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const backProjBtn = document.getElementById("back-project-btn");
+const backProjCloseBtn = document.getElementById("back-project-close-btn");
+const backProjDialog = document.getElementById("back-project-dialog");
+
+backProjBtn.addEventListener("click", () => {
+  backProjDialog.showModal();
+});
+
+backProjCloseBtn.addEventListener("click", () => {
+  backProjDialog.close();
+});
