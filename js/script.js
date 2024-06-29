0b3edb09be6134faff743aaf90b7bb48085c643b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-28 20:30:36 -0700

    logic for thanks dialog

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const backProjBtn = document.getElementById("back-project-btn");
 const backProjCloseBtn = document.getElementById("back-project-close-btn");
 const backProjDialog = document.getElementById("back-project-dialog");
 
+const continueBtns = document.querySelectorAll(".dialog-pledge>button");
+const thanksDialog = document.getElementById("thanks-dialog");
+const gotItBtn = thanksDialog.querySelector("button");
+const showModel = function () {
+  thanksDialog.showModal();
+};
 backProjBtn.addEventListener("click", () => {
   backProjDialog.showModal();
 });
@@ -9,4 +15,12 @@ backProjBtn.addEventListener("click", () => {
 backProjCloseBtn.addEventListener("click", (e) => {
   e.preventDefault();
   backProjDialog.close();
+});
+
+for (const continueBtn of continueBtns) {
+  continueBtn.addEventListener("click", showModel);
+}
+
+gotItBtn.addEventListener("click", () => {
+  thanksDialog.close();
 });
